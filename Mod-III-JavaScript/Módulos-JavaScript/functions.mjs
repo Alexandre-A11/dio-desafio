bee67164e02ada6fb058d86b78987c316a5ad800
Alexandre <alee.wxa@gmail.com>
2022-04-26 18:10:03 -0300

    feat: adiciona Módulos-JavaScript

---
+++ Mod-III-JavaScript/Módulos-JavaScript/functions.mjs
@@ -0,0 +1,13 @@
+function showAge(name, age) {
+    return `A idade de ${name} é ${age}`;
+}
+
+function showCity(name, city) {
+    return `A idade de ${name} é ${city}`;
+}
+
+function showHobby(name, hobby) {
+    return `A idade de ${name} é ${hobby}`;
+}
+
+export { showAge, showCity, showHobby };
---
+++ Mod-III-JavaScript/Módulos-JavaScript/main.mjs
@@ -0,0 +1,5 @@
+import { showAge, showCity, showHobby } from "./functions.mjs";
+
+console.log(showAge("Alexandre", 99));
+console.log(showCity("Alexandre", "Montes Claros"));
+console.log(showHobby("Alexandre", "Programar"));
