3a2d11002d7eb69157b89245d9382d40e45ef071
Alexandre <alee.wxa@gmail.com>
2022-04-27 19:03:54 -0300

    feat: update typescript

---
+++ Mod-III-JavaScript/Intro-TypeScript/index.js
@@ -0,0 +1,5 @@
+var input = document.getElementById("input");
+input.addEventListener("input", function (e) {
+    var i = e.currentTarget;
+    console.log(i.value);
+});
--- Mod-III-JavaScript/Intro-TypeScript/index.ts
+++ Mod-III-JavaScript/Intro-TypeScript/index.ts
@@ -1,1 +1,37 @@
-console.log("TypeScript");
+// // * types
+// // type IAnimal = {
+// //     nome: string;
+// //     tipo: "terrestre" | "aquático";
+// //     executarRugido(AlturaEmDecibeis: number): void;
+// // };
+
+// // * interfaces
+// interface IAnimal {
+//     nome: string;
+//     tipo: "terrestre" | "aquático";
+//     domestico: boolean;
+// }
+
+// interface IFelinos extends IAnimal {
+//     visaoNoturna: boolean;
+// }
+
+// interface ICanino extends IAnimal {
+//     porte: "pequeno" | "médio" | "grande";
+// }
+
+// type IDomestico = IFelinos | ICanino;
+
+// const animal: IDomestico = {
+//     domestico: true,
+//     nome: "Cachorro",
+//     porte: "médio",
+//     tipo: "terrestre",
+// };
+
+const input = document.getElementById("input") as HTMLInputElement;
+
+input.addEventListener("input", (e) => {
+    const i = e.currentTarget as HTMLInputElement;
+    console.log(i.value);
+});
