27a2c587c62c11de4f7cce5dcec8880362c48176
Alexandre <alee.wxa@gmail.com>
2022-04-27 21:39:52 -0300

    feat: update Intro-TypeScript

--- Mod-III-JavaScript/Intro-TypeScript/index.js
+++ Mod-III-JavaScript/Intro-TypeScript/index.js
@@ -1,5 +1,8 @@
-var input = document.getElementById("input");
-input.addEventListener("input", function (e) {
-    var i = e.currentTarget;
-    console.log(i.value);
-});
+var MeuCachorro = /** @class */ (function () {
+    function MeuCachorro(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+    return MeuCachorro;
+}());
+var cao = new MeuCachorro("Apolo", 14);
--- Mod-III-JavaScript/Intro-TypeScript/index.ts
+++ Mod-III-JavaScript/Intro-TypeScript/index.ts
@@ -29,9 +29,65 @@
 //     tipo: "terrestre",
 // };
 
-const input = document.getElementById("input") as HTMLInputElement;
+// *
+// const input = document.getElementById("input") as HTMLInputElement;
 
-input.addEventListener("input", (e) => {
-    const i = e.currentTarget as HTMLInputElement;
-    console.log(i.value);
-});
+// input.addEventListener("input", (e) => {
+//     const i = e.currentTarget as HTMLInputElement;
+//     console.log(i.value);
+// });
+
+// * Generic Types
+// function adicionaApendiceALista<T>(array: any[], valor: T) {
+//     return array.map((item) => item + valor);
+// }
+
+// adicionaApendiceALista(["1", "2", "3"], "1");
+
+// // *
+// interface IUsuario {
+//     id: string;
+//     email: string;
+//     cargo?: "funcionario" | "gerente" | "coordernador" | "supervisor";
+// }
+
+// function redirecione(usuario: IUsuario) {
+//     if (usuario.cargo) {
+//         // redirecionar(usuario.cargo);
+//     }
+
+//     // redirecionar para a área do usuário.
+// }
+
+// *
+// interface Cachorro {
+//     nome: string;
+//     idade: number;
+//     parqueFavorito?: string;
+// }
+
+// type CachorroSomenteLeitura = {
+//     +readonly [K in keyof Cachorro]-?: Cachorro[K];
+// };
+// class MeuCachorro implements Cachorro {
+//     nome;
+//     idade;
+
+//     constructor(nome, idade) {
+//         this.nome = nome;
+//         this.idade = idade;
+//     }
+// }
+
+// const cao = new MeuCachorro("Apolo", 14);
+
+// *
+interface Pessoa {
+    nome: string;
+    idade: number;
+    nacionalidade: string;
+}
+
+interface Brasileiro extends Omit<Pessoa, "nacionalidade"> {}
+
+const brasileiro: Brasileiro = {};
